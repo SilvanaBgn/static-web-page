f6f0836b81ce163be615f9fb2660bbe2a7d691c6
silvana <silvana.belen.garcia@gmail.com>
2020-07-05 01:31:16 -0300

    M002 First advances on menu

---
+++ app/js/menu.js
@@ -0,0 +1,13 @@
+/*const MENU_OPENED_PATH = app/assets/menu-opened.svg,
+MENU_CLOSED_PATH = app/assets/menu-closed.svg
+
+let menuButton = document.getElementById("menu_button");
+menuButton.addEventListener("click", toogleMenu, true);
+
+function toogleMenu(e) {
+  let menuNav = document.getElementsByTagName("nav");
+  //cambiar la imagen del botón
+  let menuButtonImg = menuButton.getElementsByTagName("img");
+  menuButtonImg.attributes["src"] = 
+  //agregarle clase .menu_opened al nav, con top y transition
+}*/
