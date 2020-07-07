6b16c8f25e5f395062426d460b074ba6d185756e
silvana <silvana.belen.garcia@gmail.com>
2020-07-07 10:02:44 -0300

    M002 Toogle menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,13 +1,19 @@
-/*const MENU_OPENED_PATH = app/assets/menu-opened.svg,
-MENU_CLOSED_PATH = app/assets/menu-closed.svg
+const MENU_OPENED_PATH = "app/assets/menu-opened.svg",
+  MENU_CLOSED_PATH = "app/assets/menu-closed.svg";
 
 let menuButton = document.getElementById("menu_button");
 menuButton.addEventListener("click", toogleMenu, true);
 
 function toogleMenu(e) {
-  let menuNav = document.getElementsByTagName("nav");
-  //cambiar la imagen del botón
-  let menuButtonImg = menuButton.getElementsByTagName("img");
-  menuButtonImg.attributes["src"] = 
-  //agregarle clase .menu_opened al nav, con top y transition
-}*/
+  let menuNav = document.getElementsByTagName("nav")[0],
+    menuButtonImg = menuButton.getElementsByTagName("img")[0];
+  const menuOpened = menuNav.classList.contains("nav_opened");
+
+  if (menuOpened) {
+    menuNav.classList.remove("nav_opened"); //update state (class) of nav
+    menuButtonImg.src = MENU_CLOSED_PATH; //update button's icon
+  } else {
+    menuNav.classList.add("nav_opened"); //update state (class) of nav
+    menuButtonImg.src = MENU_OPENED_PATH; //update button's icon
+  }
+}
