c1eb53355d96cb23073003525b5985e93d3463ad
Silvana Garcia <silvana.belen.garcia@gmail.com>
2021-10-22 02:17:00 -0300

    M009 - Contact module (#23)
    * M009 New section title css. New html, css and icons and firs approach for Contact section.
    
    * M009 Section title adaptions for mobile
    
    * M009 Min fixes to adaptions on tiles
    
    * M009 New form with labels and inputs for name, surname, email and message. New css updates and adaptions with break points for responsiveness
    
    * M009 New Whatsapp and telephone links working ok. New patterns for inputs
    
    * M009 Email configured with Emailjs
    
    * M009 Improvements on header height. Use of global css variable for the dependecies
    
    * M009 Update of font-size and colors for contact form, inputs, textarea and button.
    
    * M009 Re-order of some css by file.
    
    * M009 Clear form after sendind email
    
    * M009 Correction on anchor decoration and hover
    
    * M009 New title for inputs as warnings for submit
    
    * M009 Limit length on the amount of caracters for inputs
    
    * M009 Minor linter fixes

---
+++ app/js/contact.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("form_message");
+form.addEventListener('submit', sendMail);
+
+function sendMail(e) {
+  e.preventDefault();
+  const tempParams = {
+    name: document.getElementById("name").value,
+    surname: document.getElementById("surname").value,
+    from_email: document.getElementById("from_email").value,
+    replay_to: document.getElementById("from_email").value,
+    message: document.getElementById("message").value
+  };
+
+  form.reset();
+  emailjs.send("service_jhr1jur","template_u27q6lj", tempParams).then((res,err)=>{
+    err ? console.log("Error:", err) : console.log("Success:", res);
+  });
+}
